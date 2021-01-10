cafb35926ad9a5c04401365dde0bc72ae98fc010
SaloniSS <sxs170015@utdallas.edu>
2021-01-10 10:55:31 -0600

    Make register form functional

--- frontend/src/pages/register.js
+++ frontend/src/pages/register.js
@@ -1,75 +1,107 @@
-
-import React from "react";
-import { Link, withRouter } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, withRouter, useHistory } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
-import '../styles/styling.css';
+import "../styles/styling.css";
 
 const useStyles = makeStyles({
-    root: {
-        maxWidth: 700,
-        margin: "auto",
-    },
-    title: {
-        fontSize: 14,
-    },
-    pos: {
-        marginBottom: 12,
-    },
-    input: {
-        marginBottom: 15,
-    },
+  root: {
+    maxWidth: 700,
+    margin: "auto",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  input: {
+    marginBottom: 15,
+  },
 });
 
 function Register() {
-    const classes = useStyles();
-    return (
-        <div className="Register">
-            
-            <Card className={classes.root} variant="outlined">
-                <CardContent>
-                    <Typography variant="h5" component="h2">
-                        Cerv
-            </Typography>
-                    <Typography className={classes.pos} color="textSecondary">
-                        Register with your E-mail and phone number.
-            </Typography>
-                    <form noValidate autoComplete="off">
-                        <TextField
-                            className={classes.input}
-                            id="outlined-basic"
-                            label="E-Mail"
-                            variant="outlined"
-                        />
-                        <br />
-                        <TextField
-                            className={classes.input}
-                            id="outlined-basic"
-                            label="Phone Number"
-                            variant="outlined"
-                        />
-                        <br />
-                        <TextField
-                            className={classes.input}
-                            id="outlined-basic"
-                            label="Password"
-                            variant="outlined"
-                        />
-                        
-                    </form>
-                </CardContent>
-                <CardActions>
-                    <Link to='/home' style={{margin: "auto"}}>
-                        <button className="formBtn">Register</button>
-                    </Link>
-                </CardActions>
-            </Card>
-        </div>
-    );
+  const classes = useStyles();
+  let history = useHistory();
+  let [name, setName] = useState("");
+  let [email, setEmail] = useState("");
+  let [phone, setPhone] = useState("");
+  let [password, setPassword] = useState("");
+
+  function registerUser() {
+    const user = {
+      Name: name,
+      Email: email,
+      Phone: phone,
+      Passsword: password,
+    };
+    console.log(user);
+    history.push("/home");
+  }
+
+  return (
+    <div className="Register">
+      <Card className={classes.root} variant="outlined">
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            Cerv
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            Register with your E-mail and phone number.
+          </Typography>
+          <form noValidate autoComplete="off">
+            <TextField
+              className={classes.input}
+              id="outlined-basic"
+              label="Name"
+              variant="outlined"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+            />
+            <br />
+            <TextField
+              className={classes.input}
+              id="outlined-basic"
+              label="E-Mail"
+              variant="outlined"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+            <br />
+            <TextField
+              className={classes.input}
+              id="outlined-basic"
+              label="Phone Number"
+              variant="outlined"
+              value={phone}
+              onChange={(event) => setPhone(event.target.value)}
+            />
+            <br />
+            <TextField
+              className={classes.input}
+              id="outlined-basic"
+              label="Password"
+              variant="outlined"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </form>
+        </CardContent>
+        <CardActions>
+          <button className="formBtn" onClick={registerUser}>
+            Register
+          </button>
+          {/* <Link to="/home" style={{ margin: "auto" }}>
+            <button className="formBtn">Register</button>
+          </Link> */}
+        </CardActions>
+      </Card>
+    </div>
+  );
 }
 
 export default Register;
