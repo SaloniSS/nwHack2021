ade9637052e4b791459538814f9bdf40fcf33073
Reshmi Ranjith <rreshmiranjith@gmail.com>
2021-01-10 14:02:13 -0600

    make the nav bar more

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,10 +3,10 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import HomeWorkIcon from "@material-ui/icons/HomeWork";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import IconButton from "@material-ui/core/IconButton";
 import AddCircleIcon from "@material-ui/icons/AddCircle";
-import MenuIcon from "@material-ui/icons/Menu";
 import { Link, useHistory } from "react-router-dom";
 let GLOBAL = require("../global");
 
@@ -35,14 +35,13 @@ function Navbar(props) {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            edge="start"
+          <Link
+            to="/home"
             className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
+            style={{ marginRight: 10, color: "white" }}
           >
-            <MenuIcon />
-          </IconButton>
+            <HomeWorkIcon fontSize="large" />
+          </Link>
           <Typography variant="h6" className={classes.title}>
             {props.title}
           </Typography>
