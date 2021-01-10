4b90fe8611cd7e04274bda200e8ebd6df3acb0e5
Reshmi Ranjith <rreshmiranjith@gmail.com>
2021-01-09 23:24:29 -0600

    add job pg

---
+++ frontend/src/pages/AddJob.js
@@ -0,0 +1,66 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 600,
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  input: {
+    marginBottom: 15,
+  },
+});
+
+function AddJob() {
+  const classes = useStyles();
+
+  return (
+    <div className="dashboard">
+      <Navbar />
+      <Card className={classes.root} variant="outlined">
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            Add a Job
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            Fill out the form below to add a job to our site!
+          </Typography>
+          <form noValidate autoComplete="off">
+            <TextField
+              className={classes.input}
+              id="outlined-basic"
+              label="Title"
+              variant="outlined"
+            />
+            <br />
+            <TextField
+              multiline
+              rows={4}
+              fullWidth
+              id="outlined-basic"
+              label="Description"
+              variant="outlined"
+            />
+          </form>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Submit</Button>
+        </CardActions>
+      </Card>
+    </div>
+  );
+}
+
+export default AddJob;
