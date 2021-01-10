124937f87aa04dc28c18976e81e259fae57411be
SaloniSS <sxs170015@utdallas.edu>
2021-01-10 12:33:10 -0600

    add post works

--- frontend/src/pages/AddJob.js
+++ frontend/src/pages/AddJob.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
@@ -7,7 +7,9 @@ import CardContent from "@material-ui/core/CardContent";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import '../styles/styling.css';
+import "../styles/styling.css";
+const axios = require("axios").default;
+let GLOBAL = require("../global");
 
 const useStyles = makeStyles({
   root: {
@@ -26,6 +28,30 @@ const useStyles = makeStyles({
 
 function AddJob() {
   const classes = useStyles();
+  let [title, setTitle] = useState("");
+  let [description, setDescription] = useState("");
+  let [location, setLocation] = useState("");
+  let [value, setValue] = useState("");
+
+  const submit = async () => {
+    const posting = {
+      User: GLOBAL.userID,
+      Title: title,
+      Description: description,
+      Value: "$" + value,
+      Location: location,
+    };
+    console.log(posting);
+    await axios
+      .post("https://nwhack.wl.r.appspot.com/api/v1/postings/", posting)
+      .then(async (response) => {
+        console.log("Success");
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
   return (
     <div className="dashboard">
@@ -52,6 +78,8 @@ function AddJob() {
                 id="outlined-basic"
                 label="Title"
                 variant="outlined"
+                value={title}
+                onChange={(event) => setTitle(event.target.value)}
               />
               <br />
               <TextField
@@ -62,6 +90,8 @@ function AddJob() {
                 id="outlined-basic"
                 label="Description"
                 variant="outlined"
+                value={description}
+                onChange={(event) => setDescription(event.target.value)}
               />
               <br />
               <TextField
@@ -72,6 +102,8 @@ function AddJob() {
                 id="outlined-basic"
                 label="Address"
                 variant="outlined"
+                value={location}
+                onChange={(event) => setLocation(event.target.value)}
               />
               <br />
               <TextField
@@ -79,11 +111,15 @@ function AddJob() {
                 id="outlined-basic"
                 label="Value ($)"
                 variant="outlined"
+                value={value}
+                onChange={(event) => setValue(event.target.value)}
               />
             </form>
           </CardContent>
           <CardActions>
-            <button className="formBtn">Submit</button>
+            <button className="formBtn" onClick={submit}>
+              Submit
+            </button>
           </CardActions>
         </Card>
       </div>
