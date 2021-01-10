f7a4d54d96ff4c77daab484741c33cdc82327bde
SaloniSS <sxs170015@utdallas.edu>
2021-01-09 21:33:52 -0600

    PATCH works omfg

--- server/controller/postingController.js
+++ server/controller/postingController.js
@@ -89,3 +89,104 @@ exports.deletePosting = async (req, res, next) => {
     });
   }
 };
+
+exports.updatePosting = async (req, res, next) => {
+  try {
+    const posting = await Posting.findById(req.params.id);
+
+    if (!posting || posting.length == 0) {
+      return res.status(404).json({
+        success: false,
+        error: "No posting found",
+      });
+    }
+
+    if (req.body.User != null) {
+      await Posting.findById(req.params.id).updateOne(
+        {},
+        {
+          $set: {
+            User: req.body.User,
+          },
+        }
+      );
+    }
+
+    if (req.body.Title != null) {
+      await Posting.findById(req.params.id).updateOne(
+        {},
+        {
+          $set: {
+            Title: req.body.Title,
+          },
+        }
+      );
+    }
+
+    if (req.body.Description != null) {
+      await Posting.findById(req.params.id).updateOne(
+        {},
+        {
+          $set: {
+            Description: req.body.Description,
+          },
+        }
+      );
+    }
+
+    if (req.body.Value != null) {
+      await Posting.findById(req.params.id).updateOne(
+        {},
+        {
+          $set: {
+            Value: req.body.Value,
+          },
+        }
+      );
+    }
+
+    if (req.body.Employee != null) {
+      await Posting.findById(req.params.id).updateOne(
+        {},
+        {
+          $set: {
+            Employee: req.body.Employee,
+          },
+        }
+      );
+    }
+
+    if (req.body.Applicants != null) {
+      await Posting.findById(req.params.id).updateOne(
+        {},
+        {
+          $set: {
+            Applicants: req.body.Applicants,
+          },
+        }
+      );
+    }
+
+    if (req.body.Location != null) {
+      await Posting.findById(req.params.id).updateOne(
+        {},
+        {
+          $set: {
+            Location: req.body.Location,
+          },
+        }
+      );
+    }
+
+    return res.status(200).json({
+      success: true,
+      posting: posting,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      error: "Server Error",
+    });
+  }
+};
--- server/router/postingRouter.js
+++ server/router/postingRouter.js
@@ -5,10 +5,11 @@ const {
   getPosting,
   addPosting,
   deletePosting,
+  updatePosting,
 } = require("../controller/postingController");
 
 router.route("/").get(getPostings).post(addPosting);
 
-router.route("/:id").get(getPosting).delete(deletePosting);
+router.route("/:id").get(getPosting).delete(deletePosting).patch(updatePosting);
 
 module.exports = router;
